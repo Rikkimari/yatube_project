a76e4254c4bfe936f2f1911d9ca4390b28a1a6ad
Rikkimari <rikkimari@gmail.com>
2022-03-23 21:05:18 +0600

    Создание админки

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
-    path('', views.index),
-    path('group_list.html/', views.group_list)
+    path('', views.index, name='main_page_posts'),
+    path('group_list.html/', views.group_list, name='group_list')
 ]
